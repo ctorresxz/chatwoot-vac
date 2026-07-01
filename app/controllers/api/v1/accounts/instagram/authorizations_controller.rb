d5fe65e1360460feed6315e7178d13534fb3a95c
ctorresxz <torresxz88@gmail.com>
2026-07-01 10:59:52 -0500

    Update Instagram authorization to use force_reauth

--- app/controllers/api/v1/accounts/instagram/authorizations_controller.rb
+++ app/controllers/api/v1/accounts/instagram/authorizations_controller.rb
@@ -3,17 +3,16 @@ class Api::V1::Accounts::Instagram::AuthorizationsController < Api::V1::Accounts
   include Instagram::IntegrationHelper
 
   def create
-    # https://developers.facebook.com/docs/instagram-platform/instagram-api-with-instagram-login/business-login#step-1--get-authorization
     redirect_url = instagram_client.auth_code.authorize_url(
       {
         redirect_uri: "#{base_url}/instagram/callback",
         scope: REQUIRED_SCOPES.join(','),
-        enable_fb_login: '0',
-        force_authentication: '1',
+        force_reauth: 'true',
         response_type: 'code',
         state: generate_instagram_token(Current.account.id, params[:return_to])
       }
     )
+
     if redirect_url
       render json: { success: true, url: redirect_url }
     else
